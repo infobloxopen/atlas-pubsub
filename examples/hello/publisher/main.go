17080eacc3bc84e10ba646934f760175cb9065a5
Daniel McDonald <dmcdonald@infoblox.com>
2018-03-16 09:31:01 -0700

    clean up sloppy merge conflict resolution and remove extraneous logs

--- examples/hello/publisher/main.go
+++ examples/hello/publisher/main.go
@@ -24,29 +24,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to dial to grpc server: %v", err)
 	}
-<<<<<<< HEAD
 	for i := 0; ; i++ {
 		p := pubsubgrpc.NewPublisher(*topic, conn)
 		g := hello.Greetings[i%len(hello.Greetings)]
 
 		msg := fmt.Sprintf("%s %s", g.Message, time.Now())
 		md := map[string]string{"language": g.Language}
-		log.Printf("printing %q %v", msg, md)
+		log.Printf("publishing %q %v", msg, md)
 		p.Publish(context.Background(), []byte(msg), md)
-=======
-	p := pubsubgrpc.NewPublisher(*topic, conn)
-	for {
-		msg := fmt.Sprintf("Hello! %s", time.Now())
-		log.Printf("printing %q", msg)
-		err := p.Publish(context.Background(), []byte(msg))
-		if err != nil {
-			log.Printf("Failed to send a message")
-			log.Println(err.Error())
-		}
-<<<<<<< HEAD
->>>>>>> Create a Makefile, dockerfiles for the server and the example publisher and subscriber. Created 3 kubernetes yaml files for deployment
-=======
->>>>>>> 8e70c05260787b3699751f50ffe3d01757a6acc1
 		time.Sleep(time.Second)
 	}
 }
--- examples/hello/subscriber/main.go
+++ examples/hello/subscriber/main.go
@@ -19,51 +19,34 @@ var subscriptionID = flag.String("subID", hello.DefaultSubscriberID, "the subscr
 var languageFilter = flag.String("lang", "", "if present, will only show messages with metadata tagged for the given language")
 
 func main() {
-	log.Println("Inside Subscriber Example")
 	flag.Parse()
 	conn, err := grpc.Dial(*url, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("failed to dial to grpc server: %v", err)
 	}
-	// s := pubsubgrpc.NewSubscriber(*topic, *subscriptionID, conn)
+	s := pubsubgrpc.NewSubscriber(*topic, *subscriptionID, conn)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	md := make(map[string]string)
 	if languageFilter != nil && *languageFilter != "" {
 		md["language"] = *languageFilter
 		log.Printf("Only receiving messages written in %q", *languageFilter)
 	}
 	c, e := s.Start(context.Background(), md)
-=======
-	s := pubsubgrpc.NewSubscriber(*topic, "testroman", conn)
->>>>>>> Changed kubernetes yaml files to work with correct url and update glide to dep.
-=======
-	s := pubsubgrpc.NewSubscriber(*topic, "testroman", conn)
->>>>>>> 8e70c05260787b3699751f50ffe3d01757a6acc1
 
-	c, e := s.Start(context.Background())
 	for {
-		log.Println("Inside for loop")
 		select {
 		case msg, isOpen := <-c:
-			log.Println("Inside if msg received")
 			if !isOpen {
-				// log.Fatalln("subscription channel closed")
-
-				log.Println("Not open")
+				log.Println("subscription channel closed")
 				return
 			}
 			greeting := string(msg.Message())
 			log.Printf("received message: %q", greeting)
-
-			log.Println("Inside received message " + greeting)
 			if err := msg.Ack(); err != nil {
 				log.Fatalf("failed to ack messageID %q: %v", msg.MessageID(), err)
 			}
 		case err := <-e:
-			log.Println("Inside error")
-			log.Fatalf("encountered error reading subscription: %v", err)
+			log.Printf("encountered error reading subscription: %v", err)
 		}
 	}
 }
