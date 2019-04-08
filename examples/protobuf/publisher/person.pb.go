ba4d5ae4d5f7c27f1269f2f96a146df3b62cdf8e
Siarhei Pazdniakou <spazdniakou@infoblox.com>
2019-04-08 13:13:16 +0300

    Fixed issues

--- examples/protobuf/publisher/person.pb.go
+++ examples/protobuf/publisher/person.pb.go
@@ -3,11 +3,9 @@
 
 package main
 
-import (
-	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
-	math "math"
-)
+import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
+import math "math"
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
@@ -18,7 +16,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type EmployeeInfo struct {
 	Location             string   `protobuf:"bytes,1,opt,name=location,proto3" json:"location,omitempty"`
@@ -32,17 +30,16 @@ func (m *EmployeeInfo) Reset()         { *m = EmployeeInfo{} }
 func (m *EmployeeInfo) String() string { return proto.CompactTextString(m) }
 func (*EmployeeInfo) ProtoMessage()    {}
 func (*EmployeeInfo) Descriptor() ([]byte, []int) {
-	return fileDescriptor_4c9e10cf24b1156d, []int{0}
+	return fileDescriptor_person_cbe5ed8dfbafa8ec, []int{0}
 }
-
 func (m *EmployeeInfo) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_EmployeeInfo.Unmarshal(m, b)
 }
 func (m *EmployeeInfo) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_EmployeeInfo.Marshal(b, m, deterministic)
 }
-func (m *EmployeeInfo) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_EmployeeInfo.Merge(m, src)
+func (dst *EmployeeInfo) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_EmployeeInfo.Merge(dst, src)
 }
 func (m *EmployeeInfo) XXX_Size() int {
 	return xxx_messageInfo_EmployeeInfo.Size(m)
@@ -80,17 +77,16 @@ func (m *Person) Reset()         { *m = Person{} }
 func (m *Person) String() string { return proto.CompactTextString(m) }
 func (*Person) ProtoMessage()    {}
 func (*Person) Descriptor() ([]byte, []int) {
-	return fileDescriptor_4c9e10cf24b1156d, []int{1}
+	return fileDescriptor_person_cbe5ed8dfbafa8ec, []int{1}
 }
-
 func (m *Person) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Person.Unmarshal(m, b)
 }
 func (m *Person) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Person.Marshal(b, m, deterministic)
 }
-func (m *Person) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Person.Merge(m, src)
+func (dst *Person) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Person.Merge(dst, src)
 }
 func (m *Person) XXX_Size() int {
 	return xxx_messageInfo_Person.Size(m)
@@ -127,18 +123,18 @@ func init() {
 	proto.RegisterType((*Person)(nil), "main.Person")
 }
 
-func init() { proto.RegisterFile("person.proto", fileDescriptor_4c9e10cf24b1156d) }
+func init() { proto.RegisterFile("person.proto", fileDescriptor_person_cbe5ed8dfbafa8ec) }
 
-var fileDescriptor_4c9e10cf24b1156d = []byte{
-	// 156 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x29, 0x48, 0x2d, 0x2a,
+var fileDescriptor_person_cbe5ed8dfbafa8ec = []byte{
+	// 155 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0x29, 0x48, 0x2d, 0x2a,
 	0xce, 0xcf, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0xc9, 0x4d, 0xcc, 0xcc, 0x53, 0x72,
 	0xe0, 0xe2, 0x71, 0xcd, 0x2d, 0xc8, 0xc9, 0xaf, 0x4c, 0x4d, 0xf5, 0xcc, 0x4b, 0xcb, 0x17, 0x92,
 	0xe2, 0xe2, 0xc8, 0xc9, 0x4f, 0x4e, 0x2c, 0xc9, 0xcc, 0xcf, 0x93, 0x60, 0x54, 0x60, 0xd4, 0xe0,
 	0x0c, 0x82, 0xf3, 0x85, 0x44, 0xb8, 0x58, 0x4b, 0x32, 0x4b, 0x72, 0x52, 0x25, 0x98, 0xc0, 0x12,
 	0x10, 0x8e, 0x52, 0x1a, 0x17, 0x5b, 0x00, 0xd8, 0x5c, 0x21, 0x21, 0x2e, 0x96, 0xbc, 0xc4, 0xdc,
 	0x54, 0xa8, 0x3e, 0x30, 0x5b, 0x48, 0x80, 0x8b, 0x39, 0x31, 0x1d, 0xa2, 0x83, 0x35, 0x08, 0xc4,
-	0x14, 0x32, 0xe3, 0xe2, 0x49, 0x45, 0xb2, 0x51, 0x82, 0x59, 0x81, 0x51, 0x83, 0xdb, 0x48, 0x48,
-	0x0f, 0xe4, 0x1c, 0x3d, 0x64, 0xb7, 0x04, 0xa1, 0xa8, 0x4b, 0x62, 0x03, 0x3b, 0xdb, 0x18, 0x10,
-	0x00, 0x00, 0xff, 0xff, 0xaa, 0xe5, 0xd6, 0xee, 0xc6, 0x00, 0x00, 0x00,
+	0x14, 0x32, 0xe3, 0xe2, 0x49, 0x45, 0xb2, 0x51, 0x82, 0x19, 0x28, 0xc5, 0x6d, 0x24, 0xa4, 0x07,
+	0x72, 0x8e, 0x1e, 0xb2, 0x5b, 0x82, 0x50, 0xd4, 0x25, 0xb1, 0x81, 0x9d, 0x6d, 0x0c, 0x08, 0x00,
+	0x00, 0xff, 0xff, 0xaa, 0xe5, 0xd6, 0xee, 0xc6, 0x00, 0x00, 0x00,
 }
